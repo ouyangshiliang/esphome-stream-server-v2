46d654d58130cd41c097c66d58f79511a78f45ed
ouyangshiliang <ouyangshiliang223@gmail.com>
2022-12-14 09:16:35 +0800

    Update binary_sensor.h

--- components/stream_server/binary_sensor.h
+++ components/stream_server/binary_sensor.h
@@ -17,7 +17,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "/esphome/esphome/components/binary_sensor/binary_sensor.h"
 #include "stream_server.h"
 
 class StreamServerBinarySensor : public esphome::binary_sensor::BinarySensor, public esphome::Component {
